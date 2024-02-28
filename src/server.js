025bd55118785733db1af164300dce0053b176e0
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-02-28 15:27:39 -0300

    start: Starting the creation of a HTTP server

---
+++ src/server.js
@@ -0,0 +1,52 @@
+import http from 'node:http'
+
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso no back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+
+// GET /users => Buscando usuários do back-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/Resposta) => Metadados
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
+    return res.end('Criação de usuários')
+  }
+
+  return res.end('Hello World!')
+  ;('')
+})
+
+server.listen(3333)
