23a7dfc5979055cd266f64afdb194e64881b7385
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-03-01 17:33:17 -0300

    feat: Add the create user feat and creating a middleware to converse data to JSON

--- src/server.js
+++ src/server.js
@@ -1,45 +1,24 @@
 import http from 'node:http'
-
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso no back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso no back-end
-
-// GET /users => Buscando usuários do back-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/Resposta) => Metadados
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
+  await json(req, res)
+
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com'
+      name,
+      email
     })
 
     return res.writeHead(201).end()
