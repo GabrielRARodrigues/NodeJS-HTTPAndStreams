69ed05a554425752334f27e131919b0b837c7879
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-03-03 20:58:03 -0300

    Persisting the data sent by the user in the 'db.json' file through the 'database.js' file and separating the application routes in the 'routes.js' file.

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 
-const users = []
+// Query Parameters: URL Stateful => Filtros, paginação, não-obrigatórios
+// Route Parameters: Identificação de recurso
+// Request Body: Envio de informações de um fomulário (HTTPs)
+
+// http://localhost:3333/users?userId=1&name=Gabriel
+
+// GET http://localhost:3333/users/1
+// DELETE http://localhost:3333/users/1
+
+// POST http://localhost:3333/users
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    return res.end(JSON.stringify(users))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    users.push({
-      id: 1,
-      name,
-      email
-    })
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
